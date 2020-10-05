09de5b618a4e0262e3390bf73106bf312d6fd00e
Martin Guillon <martin.guillon@akylas.fr>
2020-10-05 21:40:46 +0200

    chore: everything working

--- plugin/platforms/android/java/com/nativescript/push/PushMessagingServiceListener.java
+++
@@ -1,9 +0,0 @@
-package com.nativescript.push;
-
-import com.google.firebase.messaging.RemoteMessage;
-
-public interface PushMessagingServiceListener {
-  void success(Object data, RemoteMessage.Notification notif);
-  void success(Object data);
-  void error(Object data);
-}
--- src/scripts/after-prepare.ts
+++ src/scripts/after-prepare.ts
@@ -1,14 +1,35 @@
 import * as path from 'path';
 import * as fs from 'fs';
 import * as plist from 'plist';
-import { exec } from 'child_process';
-export default function ($logger, $projectData, hookArgs) {
+import { exec, spawn } from 'child_process';
+module.exports = function ($logger, $projectData, hookArgs) {
     return new Promise(function (resolve, reject) {
         const platformFromHookArgs = hookArgs && (hookArgs.platform || (hookArgs.prepareData && hookArgs.prepareData.platform));
         const platform = (platformFromHookArgs || '').toLowerCase();
 
         /* Handle preparing of Google Services files */
         if (platform === 'android') {
+            console.log('generateLicenseReport', $projectData.projectDir);
+            const command = spawn('./gradlew', ['generateLicenseReport'], {
+                cwd: path.join($projectData.projectDir, 'platforms/android'),
+            });
+
+            command.stdout.on('data', (data) => {
+                $logger.info('@nativescript-community/licenses: ' + data);
+            });
+
+            command.stderr.on('data', (data) => {
+                $logger.error('@nativescript-community/licenses: ' + data);
+            });
+
+            command.on('close', (code) => {
+                console.log(`child process exited with code ${code}`);
+                if (code === 0) {
+                    resolve();
+                } else {
+                    reject(new Error('generateLicenseReport failed'));
+                }
+            });
         } else if (platform === 'ios') {
             const licenseUrls = {
                 'Apache 2.0': 'http://www.apache.org/licenses/LICENSE-2.0',
@@ -16,13 +37,12 @@ export default function ($logger, $projectData, hookArgs) {
                 mit: 'http://www.apache.org/licenses/LICENSE-2.0',
                 BSD: 'https://opensource.org/licenses/BSD-3-Clause',
             };
-            const metadaFile = fs
-                .readdirSync(path.join($projectData.projectDir, 'platforms/ios/Pods'))
-                .filter((s) => s.endsWith('-metadata.plist'))[0];
+            const dirPath = path.join($projectData.projectDir, 'platforms/ios/Pods');
+            const metadaFile = fs.readdirSync(dirPath).filter((s) => s.endsWith('-metadata.plist'))[0];
             if (!metadaFile) {
                 return reject(new Error('Could not find the Pods metadata file'));
             }
-            const obj = plist.parse(fs.readFileSync(metadaFile, 'utf8'));
+            const obj = plist.parse(fs.readFileSync(path.join(dirPath, metadaFile), 'utf8'));
             const result = {
                 dependencies: obj.specs.map((spec) => ({
                     moduleName: spec.name,
@@ -33,21 +53,12 @@ export default function ($logger, $projectData, hookArgs) {
                     moduleLicenseUrl: licenseUrls[spec.licenseType],
                 })),
             };
-            fs.writeFileSync(path.join($projectData.appDirectoryPath, 'ios/licenses.json'), JSON.stringify(result));
+            const outPath = path.join($projectData.appDirectoryPath, 'ios');
+            if (!fs.existsSync(outPath)) {
+                fs.mkdirSync(outPath);
+            }
+            fs.writeFileSync(path.join(outPath, 'licenses.json'), JSON.stringify(result));
             resolve();
-        } else {
-            exec(
-                './gradlew generateLicenseReport',
-                {
-                    cwd: path.join($projectData.projectDir, '/platforms/android'),
-                },
-                function (error, stdout, stderr) {
-                    if (error) {
-                        reject(error);
-                    }
-                    resolve();
-                }
-            );
         }
     });
-}
+};
